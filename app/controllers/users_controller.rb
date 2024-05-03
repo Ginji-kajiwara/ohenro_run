d778dbe041fb1d022d9105c087c584671fb3de8e
Ginji-kajiwara <130469578+Ginji-kajiwara@users.noreply.github.com>
2024-05-03 13:35:33 +0900

    add:新規登録時のエラーメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'ユーザー登録が完了しました'
-      redirect_to login_path
+      redirect_to current_location_show_path
     else
       render :new
     end
