a8b2091b01845c1537608308e6b18320389accf1
Ginji-kajiwara <130469578+Ginji-kajiwara@users.noreply.github.com>
2024-07-07 15:59:25 +0900

    fix:OmniAuthリダイレクトURI

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,8 @@
-# frozen_string_literal: true
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
     scope: 'email,profile',
     prompt: 'select_account',
-    image_aspect_ratio: 'square',
-    image_size: 50,
-    access_type: 'offline'
+    access_type: 'offline',
+    redirect_uri: 'https://www.ohenro-running.com/auth/google_oauth2/callback'
   }
 end
